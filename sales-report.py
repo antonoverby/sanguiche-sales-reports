529982af0a79fee21797c0ca66048f0ec0555088
Anton Overby <aoverby.devworks@gmail.com>
2023-06-11 23:57:49 -0500

    added date range input to modify graphs connected to sales dataframe

--- sales-report.py
+++ sales-report.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-st.set_page_config(layout='wide', initial_sidebar_state="collapsed")
-sales = pd.read_csv("data\sales-6.5.23-to-6.11.23.csv")
+from datetime import datetime
 
+st.set_page_config(layout='wide', initial_sidebar_state="collapsed")
+
+sales = pd.read_csv("data/sales-6.5.23-to-6.11.23.csv")
+sales['Date'] = pd.to_datetime(sales['Date']).dt.date
+
+# Date range picker
+start_date = datetime(2023, 6, 1)
+end_date = datetime.now()
+date_input = st.date_input("Select a date range", value=(start_date, end_date))
+if len(date_input) != 2:
+    st.stop()
+start_input = date_input[0]
+end_input = date_input[1]
+    
+# Define sales DF as between the date ranges selected
+sales = sales.loc[sales['Date'].between(start_input, end_input)]
 sandwich_sales = sales[sales['Category'] == 'Sandwiches']
 sides = sales[sales['Category'] == 'Sides']
 addon_sales = sales[sales['Category'] == 'Add Ons']
@@ -19,8 +34,6 @@ def sales_by_item_barchart(df):
     fig.update_traces(textposition='outside')
     return st.plotly_chart(fig, use_container_width=True)
 
-
-
 col1, col2, col3 = st.columns(3)
 with col1:
     st.subheader("Sandwich sales totals")
@@ -31,6 +44,3 @@ with col2:
 with col3:
     st.subheader("Add-on sales totals")
     sales_by_item_barchart(addon_sales)
-
-
-
